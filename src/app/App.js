8ac3ae047089ffe17e1f86e53f7a691c1f78c7e3
blacktiny <blacktiny126@hotmail.com>
2020-11-10 09:23:39 -0800

    integrate rsuite and some dependency packages

---
+++ src/app/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { Container, Header, Content } from 'rsuite';
+import RouteWithSubRoutes from './global/components/RouteWithSubRoutes';
+import routes from './global/utils/Routes';
+import 'rsuite/lib/styles/index.less';
+import './global/styles/globalStyle.less';
+import './global/styles/App.less';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Container className="App">
+        <Header className="App-header"></Header>
+        <Content>
+          <Switch>
+            {routes.map((route, i) => {
+              return <RouteWithSubRoutes key={i} {...route} />;
+            })}
+          </Switch>
+        </Content>
+      </Container>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  // eslint-disable-next-line no-undef
   document.getElementById('root')
 );
 

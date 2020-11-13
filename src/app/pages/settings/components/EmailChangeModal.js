a67ea3d69295a889c571b890da533c47bee76ce1
blacktiny <blacktiny126@hotmail.com>
2020-11-13 00:49:22 -0800

    add Email and Phone Change modal

---
+++ src/app/pages/settings/components/EmailChangeModal.js
@@ -0,0 +1,202 @@
+import React, { useMemo, useState } from 'react';
+import { Modal } from 'rsuite';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ButtonItem, TextInputItem, MDPPSModal } from '../../../common/components';
+import { changeEmail, finishEmailChange } from '../../../common/actions'
+
+function EmailInputPage(props) {
+  const { onNext, onClosed } = props;
+  const [changeEmailAddr, setChangeEmailAddr] = useState('')
+  // const [isValidated, setIsValidated] = useState(false)
+
+  return (
+    <div className="Email-change-input">
+      <Modal.Header>
+        <Modal.Title>Change Email Address</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p style={{ fontSize: 14, lineHeight: '25px', color: 'black' }}>
+          Enter the new email address you would like to associate with your 
+          account below. We’ll send a One Time Password (OTP) to that address. 
+          We recommend using your company email address for verification and security.
+        </p>
+        <TextInputItem
+          value={changeEmailAddr}
+          placeholder={'Enter your new email address'}
+          errorMsg={'Please enter a valid email address'}
+          onChange={setChangeEmailAddr}
+          // doValidate={isValidated}
+          required
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <div className="btn-group">
+          <ButtonItem
+            className="Default-btn"
+            appearance="default"
+            title="Cancel"
+            onClick={() => onClosed()}
+          />
+          <ButtonItem
+            className="Save-btn"
+            appearance="primary"
+            title="Next"
+            onClick={() => onNext(changeEmailAddr)}
+          />
+        </div>
+      </Modal.Footer>
+    </div>
+  )
+}
+
+export function OTPCodeInputPage(props) {
+  const { onNext, onClosed } = props;
+  const [otpCode, setOtpCode] = useState('')
+  // const [isValidated, setIsValidated] = useState(false)
+
+  return (
+    <div className="OTP-change-input">
+      <Modal.Header>
+        <Modal.Title>Confirm Your Request</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p style={{ fontSize: 14, lineHeight: '25px', color: 'black', marginBottom: 30 }}>
+          For your security we need to authenticate your request. We`ve sent a One 
+          Time Password (OTP) to <span style={{ fontWeight: 'bold' }}>you@companyname.com</span>. Please enter it below.
+        </p>
+        <TextInputItem
+          title={'Enter OTP'}
+          value={otpCode}
+          errorMsg={'Please check your inbox and enter the OTP password given'}
+          onChange={setOtpCode}
+          // doValidate={isValidated}
+          extraContentEle={(
+            <div className="Input-description">Next OTP can be sent in 0:29</div>
+          )}
+          required
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <div className="btn-group">
+          <ButtonItem
+            className="Default-btn"
+            appearance="default"
+            title="Cancel"
+            onClick={() => onClosed()}
+          />
+          <ButtonItem
+            className="Save-btn"
+            appearance="primary"
+            title="Confirm"
+            onClick={() => onNext(otpCode)}
+          />
+        </div>
+      </Modal.Footer>
+    </div>
+  )
+}
+
+export function FinishPage(props) {
+  const { onClosed } = props;
+
+  return (
+    <div className="Finish">
+      <Modal.Header>
+        <Modal.Title>Thank You!</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p style={{ fontSize: 14, lineHeight: '25px', color: 'black', marginBottom: 30 }}>
+          Your email address / phone number has been successfully changed.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <div className="btn-group">
+          <ButtonItem
+            className="Default-btn"
+            appearance="default"
+            title="Close"
+            onClick={() => onClosed()}
+          />
+        </div>
+      </Modal.Footer>
+    </div>
+  )
+}
+
+function EmailChangeModal(props) {
+  const {
+    show,
+    onClosed,
+    profile: {
+      emailChangeStep
+    },
+    changeEmail,
+    finishEmailChange
+  } = props;
+
+  const modalContent = useMemo(() => {
+    switch (emailChangeStep) {
+      case 'email':
+        return (
+          <EmailInputPage
+            onNext={
+              email => changeEmail({
+                step: 'email',
+                email
+              })
+            }
+            onClosed={onClosed}
+          />
+        )
+      case 'otp':
+        return (
+          <OTPCodeInputPage
+            onNext={
+              otpCode => changeEmail({
+                step: 'otp',
+                otpCode
+              })
+            }
+            onClosed={onClosed}
+          />
+        )
+      case 'finish':
+        return (
+          <FinishPage onClosed={
+            () => {
+              finishEmailChange()
+              onClosed()
+            }
+          }/>
+        )
+      default:
+        return <>Empty</>
+    }
+  }, [emailChangeStep])
+
+  return (
+    <MDPPSModal show={show} onClosed={onClosed}>
+      <div className="Email-change">
+        {modalContent}
+      </div>
+    </MDPPSModal>
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    profile: state.profile,
+  };
+}
+
+export default connect(mapStateToProps, dispatch => ({
+  dispatch,
+  ...bindActionCreators(
+    {
+      changeEmail,
+      finishEmailChange
+    },
+    dispatch
+  ),
+}))(EmailChangeModal);
---
+++ src/app/pages/settings/components/PhoneChangeModal.js
@@ -0,0 +1,128 @@
+import React, { useMemo, useState } from 'react';
+import { Modal } from 'rsuite';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ButtonItem, PhoneNumberInputItem, MDPPSModal } from '../../../common/components';
+import { OTPCodeInputPage, FinishPage } from './EmailChangeModal';
+import { changeEmail, finishEmailChange } from '../../../common/actions';
+
+function PhoneInputPage(props) {
+  const { onNext, onClosed } = props;
+  const [ phoneNumber, setPhoneNumber] = useState('')
+  // const [isValidated, setIsValidated] = useState(false)
+
+  return (
+    <div className="Email-change-input">
+      <Modal.Header>
+        <Modal.Title>Change Phone Number</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p style={{ fontSize: 14, lineHeight: '25px', color: 'black' }}>
+          Enter the new phone number you would like to associate with your 
+          account below. We’ll send a One Time Password (OTP) to that number.
+        </p>
+        <PhoneNumberInputItem
+          value={phoneNumber}
+          placeholder={'Enter your new phone number'}
+          errorMsg={'Please enter a valid phone number'}
+          onChange={setPhoneNumber}
+          // doValidate={isValidated}
+          required
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <div className="btn-group">
+          <ButtonItem
+            className="Default-btn"
+            appearance="default"
+            title="Cancel"
+            onClick={() => onClosed()}
+          />
+          <ButtonItem
+            className="Save-btn"
+            appearance="primary"
+            title="Next"
+            onClick={() => onNext(phoneNumber)}
+          />
+        </div>
+      </Modal.Footer>
+    </div>
+  )
+}
+
+function PhoneChangeModal(props) {
+  const {
+    show,
+    onClosed,
+    profile: {
+      emailChangeStep
+    },
+    changeEmail,
+    finishEmailChange
+  } = props;
+
+  const modalContent = useMemo(() => {
+    switch (emailChangeStep) {
+      case 'email':
+        return (
+          <PhoneInputPage
+            onNext={
+              email => changeEmail({
+                step: 'email',
+                email
+              })
+            }
+            onClosed={onClosed}
+          />
+        )
+      case 'otp':
+        return (
+          <OTPCodeInputPage
+            onNext={
+              otpCode => changeEmail({
+                step: 'otp',
+                otpCode
+              })
+            }
+            onClosed={onClosed}
+          />
+        )
+      case 'finish':
+        return (
+          <FinishPage onClosed={
+            () => {
+              finishEmailChange()
+              onClosed()
+            }
+          }/>
+        )
+      default:
+        return <>Empty</>
+    }
+  }, [emailChangeStep])
+
+  return (
+    <MDPPSModal show={show} onClosed={onClosed}>
+      <div className="Email-change">
+        {modalContent}
+      </div>
+    </MDPPSModal>
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    profile: state.profile,
+  };
+}
+
+export default connect(mapStateToProps, dispatch => ({
+  dispatch,
+  ...bindActionCreators(
+    {
+      changeEmail,
+      finishEmailChange
+    },
+    dispatch
+  ),
+}))(PhoneChangeModal);
